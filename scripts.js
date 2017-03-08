7f9ae43e6ad482abd6999bc85f7e2a8e0dbc4d7f
Patricia Santana <psantana.student@hsfi.us>
2017-03-08 19:40:37 +0000

    wored on javascript a bit more

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
 var generateComputerChoice = function() {
-    
-    var rps = ["rock", "paper", "scissors"]
+
+    var rps = ["rock", "paper", "scissors"];
+    var random = Math.floor(Math.random() * 2)
+    var AI_choice = rps[random];
+    return AI_choice;
+};
+
+var pickWinner = function(userchoice, computerChoice) {
+    console.log("user choice: " + userChoice);
+    console.log("computer choice: " + computerChoice);
+    if (userChoice === computerChoice) {
+        console.log("draw");
+    }
+    else if (userChoice === "rock" && computerChoice === "scissors" || userChoice === "paper" && computerChoice === "rock" || userChoice === "scissors" && computerChoice === "paper") {
+        console.log('user wins')
+    }
+    else {
+        console.log('AI wins');
+    }
 }
